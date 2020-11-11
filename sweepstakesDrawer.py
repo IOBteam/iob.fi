6d22fcff18092b8ea637642fc476a2ac2b714eb7
IOB <37628385+IOBteam@users.noreply.github.com>
2020-11-11 11:41:37 +0800

    Update sweepstakesDrawer.py

--- sweepstakesDrawer.py
+++ sweepstakesDrawer.py
@@ -1,5 +1,8 @@
 import random
 
+second = [2, 3, 4, 5, 6]
+third = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
 addresses = open("Drawing #1 Qualified.txt", 'r')
 addy = addresses.readlines()
 addy = [x.strip() for x in addy]
@@ -17,14 +20,34 @@ for a in addy:
         break
     else:
         winner = addy[random.randint(0, len(addy) - count)]
-        winnersAddy.append(winner)
-        addy.remove(winner)
-        Winners.write(winner + '\n')
-        count += 1
+        if count == 1:
+            print('--- First Place ---')
+            print(f'1. {winner}')
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+        elif count in second:
+            if count == 2:
+                print('--- Second Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+        elif count in third:
+            if count == 7:
+                print('--- Third Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+
 Winners.close()
 
 print('Finished Drawing 1st 16 winners...')
-
+input('Press enter to draw second drawing.')
 
 addresses = open("Drawing #2 Qualified.txt", 'r')
 addy = addresses.readlines()
@@ -42,13 +65,76 @@ for a in addy:
         break
     else:
         winner = addy[random.randint(0, len(addy) - count)]
-        if winner in winnersAddy:
-            continue
-        else:
+        if count == 1:
+            print('--- First Place ---')
+            print(f'1. {winner}')
             winnersAddy.append(winner)
             addy.remove(winner)
             Winners.write(winner + '\n')
             count += 1
+        elif count in second:
+            if count == 2:
+                print('--- Second Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+        elif count in third:
+            if count == 7:
+                print('--- Third Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+
 Winners.close()
 
 print('Finished Drawing 2nd 16 winners...')
+input('Press enter to draw third drawing.')
+
+addresses = open("Token Request Qualified.txt", 'r')
+addy = addresses.readlines()
+addy = [x.strip() for x in addy]
+addresses.close()
+Winners = open('Winners #3.txt', 'a')
+
+
+print(f"There are a total of {len(addy)} participants.")
+print('Drawing 3rd 16 winners...')
+
+count = 1
+
+for a in addy:
+    if not count < 17:
+        break
+    else:
+        winner = addy[random.randint(0, len(addy) - count)]
+        if count == 1:
+            print('--- First Place ---')
+            print(f'1. {winner}')
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+        elif count in second:
+            if count == 2:
+                print('--- Second Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+        elif count in third:
+            if count == 7:
+                print('--- Third Place ---')
+            print(f"{count}. {winner}")
+            winnersAddy.append(winner)
+            addy.remove(winner)
+            Winners.write(winner + '\n')
+            count += 1
+
+Winners.close()
+
+print('Finished Drawing 3rd 16 winners...')
